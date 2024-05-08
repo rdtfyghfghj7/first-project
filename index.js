c4e55c39b2f02dda506278b227c1f314862ad8bc
rdtfyghfghj7 <js1dada3463@hotmail.com>
2024-05-08 12:59:24 +0700

    aptos sdk project

--- index.js
+++ index.js
@@ -1,19 +1,4 @@
-function LRUCache(capacity) {
-  this.capacity = capacity;
-  this.cache = new Map();
+function maxDepth(root) {
+  if (!root) return 0;
+  return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
 }
-LRUCache.prototype.get = function (key) {
-  if (!this.cache.has(key)) return -1;
-  const value = this.cache.get(key);
-  this.cache.delete(key);
-  this.cache.set(key, value);
-  return value;
-};
-LRUCache.prototype.put = function (key, value) {
-  if (this.cache.has(key)) this.cache.delete(key);
-  if (this.cache.size === this.capacity) {
-    const firstKey = this.cache.keys().next().value;
-    this.cache.delete(firstKey);
-  }
-  this.cache.set(key, value);
-};
