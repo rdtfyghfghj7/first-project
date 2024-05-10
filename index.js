404bb64543db85e767eb9eafaca5bc6c9ed51bab
rdtfyghfghj7 <js1dada3463@hotmail.com>
2024-05-10 19:06:07 +0700

    update sdk task

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-const word = "hello";
-const reversedWord = word.split("").reverse().join("");
-console.log(reversedWord); // olleh
+const binarySearch = (arr, target) => {
+  let left = 0;
+  let right = arr.length - 1;
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    if (arr[mid] === target) {
+      return mid;
+    } else if (arr[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+  return -1;
+};
