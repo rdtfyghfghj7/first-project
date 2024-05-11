f67cb0521e7ef3756c1f11f1b01f4f0bba4eec5a
rdtfyghfghj7 <js1dada3463@hotmail.com>
2024-05-11 07:12:26 +0700

    demo task

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
-const binarySearch = (arr, target) => {
-  let left = 0;
-  let right = arr.length - 1;
-  while (left <= right) {
-    const mid = Math.floor((left + right) / 2);
-    if (arr[mid] === target) {
-      return mid;
-    } else if (arr[mid] < target) {
-      left = mid + 1;
-    } else {
-      right = mid - 1;
+function exist(board, word) {
+  const rows = board.length;
+  const cols = board[0].length;
+  const dfs = (i, j, index) => {
+    if (index === word.length) return true;
+    if (i < 0 || i >= rows || j < 0 || j >= cols || board[i][j] !== word[index])
+      return false;
+    const temp = board[i][j];
+    board[i][j] = "";
+    const found =
+      dfs(i + 1, j, index + 1) ||
+      dfs(i - 1, j, index + 1) ||
+      dfs(i, j + 1, index + 1) ||
+      dfs(i, j - 1, index + 1);
+    board[i][j] = temp;
+    return found;
+  };
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      if (board[i][j] === word[0] && dfs(i, j, 0)) return true;
     }
   }
-  return -1;
-};
+  return false;
+}
