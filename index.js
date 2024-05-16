9d333d01aaa1b1c173bdb5a6aceef82e94d41e74
rdtfyghfghj7 <js1dada3463@hotmail.com>
2024-05-16 20:35:05 +0700

    hello commit

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-const linearSearch = (arr, target) => {
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === target) {
-      return i;
+function findMaxLength(nums) {
+  const map = new Map();
+  map.set(0, -1);
+  let count = 0;
+  let maxLength = 0;
+  for (let i = 0; i < nums.length; i++) {
+    count += nums[i] === 1 ? 1 : -1;
+    if (map.has(count)) {
+      maxLength = Math.max(maxLength, i - map.get(count));
+    } else {
+      map.set(count, i);
     }
   }
-  return -1;
-};
+  return maxLength;
+}
